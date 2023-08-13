b56d46454560844708d51bee8a94d084446c4df6
Taloqq <antte.alatalo@hotmail.com>
2023-08-13 21:19:22 +0300

    ykioski

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ import { Chakra_Petch, Inter } from 'next/font/google';
 import { inter, robotoCursive } from './fonts'
 import google_play_badge from '../images/google-play-badge.png'
 import app_store_badge from '../images/apple-badge.svg'
-
+import ykioski_logo from '../images/ymparisto-kioski.svg'
 
 export default function Home() {
 
@@ -32,10 +32,10 @@ export default function Home() {
               <div style={{paddingTop: 50}}>Duty is an application that lets you find and hire people to do small work for you - or find work for yourself</div>
               <div style={{color: '#ffee0c'}}>to earn money.</div>
               <div className={styles.app_links}>
-                <a href='https://play.google.com/store/apps/details?id=com.devolution.duty'>
+                <a target='_blank' href='https://play.google.com/store/apps/details?id=com.devolution.duty'>
                   <Image className={styles.google_play_badge} alt='Get it on Google Play' src={google_play_badge}/>
                 </a>
-                <a href='https://apps.apple.com/fi/app/duty/id6444645784'>
+                <a target='_blank' href='https://apps.apple.com/fi/app/duty/id6444645784'>
                   <Image className={styles.app_store_badge} alt='Download on App store' src={app_store_badge}/>
                 </a>
               </div>
@@ -44,9 +44,21 @@ export default function Home() {
         </div>
 
         <div className={styles.ymparisto_kioski}>
-          <div className={styles.bg_overlay} />
+          <div className={styles.white_overlay} />
           <section className={styles.project_container}>
-            <div style={{height: 800}} />
+            <Image src={ykioski_logo} alt='Ymparistö kioski' className={styles.ykioski_logo} />
+            <div className={styles.desc_white}>
+            <h3 className={inter.className} style={{color: '#73ab5c', paddingBottom: 5}}>Environmentally friendly farming</h3>
+            <div style={{paddingTop: 50, color: 'black', fontSize: 22}}>
+              Ympäristökioski is an application - that is part of a bigger project, aiming for sustainable farming practices
+              and increased collaboration between farms. Ympäristökioski suggests environmentally friendly farming solutions for farmers,
+              based their farms&apos; properties.
+            </div>
+            <a target='_blank' href='https://app.ymparistokioski.fi/login' className={styles.ykioski_link}>
+              Website  &#8250;
+            </a>
+            </div>
+ 
           </section>
         </div>
     </main>
